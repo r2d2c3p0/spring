3b92317afc5d59935ec117d910e68895a43c2a28
r2d2c3p0 <r2d2c3p0@users.noreply.github.com>
2018-04-18 23:10:58 -0500

    pas-poc/gc/email update.

---
+++ pas-poc/src/main/java/org/r2d2c3p0/pas/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package org.r2d2c3p0.pas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+	
+	private static final String RESPONSE = "Hello from Shane's PCF PAS container!";
+
+    @RequestMapping(value = {"/**/*","/"}, method = RequestMethod.GET)
+    @ResponseBody
+    public String justSayIt(){
+        return RESPONSE;
+    }
+
+}
